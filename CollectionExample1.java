568b5b51715a534da0abaa3e55a61bb06dc681d4
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-07-02 16:49:20 -0500

    Added CollectionExample1

---
+++ CollectionExample1.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+
+class CollectionExample1
+{
+public static void main(String[] arg)
+{
+ArrayList<String> al=new ArrayList<String>();
+al.add("ramu");
+al.add("Hari");
+al.add("Madhu");
+al.add("Ragu");
+System.out.println(al);
+}
+}
