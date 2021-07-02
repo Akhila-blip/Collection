d208ef0c2b65d5c949fd91f9174adc87353028d2
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-07-02 17:18:39 -0500

    Added CollectionVectorEx program

---
+++ CollectionVectorEx.java
@@ -0,0 +1,15 @@
+import java.util.*;  
+public class CollectionVectorEx
+{  
+public static void main(String args[])
+{  
+Vector<Integer> v=new Vector<Integer>();  
+v.add(4);  
+v.add(8);  
+v.add(5);  
+v.add (6);
+System.out.println(v);
+System.out.println("current capacity of v  is" +v.capacity());
+  
+}  
+}  
