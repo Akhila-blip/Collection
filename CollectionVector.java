964c73c0a0586d7bcc4c7c3e19e222af2aa6784d
Akhila-blip <84837054+Akhila-blip@users.noreply.github.com>
2021-07-02 17:13:08 -0500

    Added CollectionVector program

---
+++ CollectionVector.java
@@ -0,0 +1,16 @@
+import java.util.*;  
+public class CollectionVector
+{  
+public static void main(String args[])
+{  
+Vector<String> v=new Vector<String>();  
+v.add("Amrutha");  
+v.add("Arpitha");  
+v.add("gowri");  
+v.add("Seetha");  
+System.out.println(v);
+System.out.println("current capacity of v  is" +v.capacity());
+  
+}  
+}  
+ 
